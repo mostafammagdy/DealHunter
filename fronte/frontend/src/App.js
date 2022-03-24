f1be80abbeea7be7535f8bba9d43a24036763403
mostafammagdy <mostafa2@yorku.ca>
2022-03-24 12:02:29 -0400

    added functioning Searchbar on item Name

--- fronte/frontend/src/App.js
+++ fronte/frontend/src/App.js
@@ -9,7 +9,6 @@ function App() {
     <div className="App">
       This is our app.
       <AppBar />
-      <ItemComponent />
     </div>
   );
 }
--- fronte/frontend/src/components/ItemComponent.js
+++ fronte/frontend/src/components/ItemComponent.js
@@ -1,10 +1,12 @@
 import React, { useReducer, useState } from "react";
+import Appbar from "./AppBar";
 
 class ItemComponent extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       items: [],
+      searchedItems: [],
       DataisLoaded: false,
     };
   }
@@ -25,29 +27,45 @@ class ItemComponent extends React.Component {
       });
   }
 
-  render() {
+  render(props) {
     const { DataisLoaded, items } = this.state;
+
     if (!DataisLoaded)
       return (
         <div>
           <h1>Please ewait...</h1>
         </div>
       );
+
     return (
       <div className="item-container">
         <h1> featured items </h1>
-        {items.map((item) => (
-          <div className="card">
-            <img
-              src="https://m.media-amazon.com/images/I/21DejQuoT2L.jpg"
-              alt=""
-            />
-            <h3>{item.brand}</h3>
-            <p>{item.description}</p>
-            <h3>{item.name}</h3>$$:{item.price}, Item_Quantity: {item.quantity}
-            <h3>{item.type}</h3>
-          </div>
-        ))}
+        <h1>{this.props.inputText}</h1>
+        {items
+          .filter((val) => {
+            if (this.props.inputText == "") {
+              return val;
+            } else if (
+              val.name
+                .toLowerCase()
+                .includes(this.props.inputText.toLowerCase())
+            ) {
+              return val;
+            }
+          })
+          .map((item) => (
+            <div className="card">
+              <img
+                src="https://m.media-amazon.com/images/I/21DejQuoT2L.jpg"
+                alt=""
+              />
+              <h3>{item.brand}</h3>
+              <p>{item.description}</p>
+              <h3>{item.name}</h3>$$:{item.price}, Item_Quantity:{" "}
+              {item.quantity}
+              <h3>{item.type}</h3>
+            </div>
+          ))}
       </div>
     );
   }
